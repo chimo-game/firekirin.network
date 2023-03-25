2dfcbba53fd90f68dc365d14f954725c5a1a2bf4
tolapao <62795957+XANuN@users.noreply.github.com>
2023-03-25 21:58:30 +0700

    added

---
+++ assets/js/change_redirect_link.js
@@ -0,0 +1,7 @@
+// select all links with class "my-link"
+const links = document.querySelectorAll('.my-link');
+
+// loop through each link and change its URL
+for (let i = 0; i < links.length; i++) {
+    links[i].href = "https://ongamerguide.com/category/online-fish-casino";
+}
