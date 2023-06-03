6bd1e47f6dcb93487bf7989ff4928a5c9af96128
tolapao <paotola10@gmail.com>
2023-06-03 11:02:44 +0700

    a

--- assets/js/change_redirect_link.js
+++ assets/js/change_redirect_link.js
@@ -3,5 +3,5 @@ const links = document.querySelectorAll('.my-link');
 
 // loop through each link and change its URL
 for (let i = 0; i < links.length; i++) {
-    links[i].href = "https://ongamerguide.com/category/online-fish-casino";
+    links[i].href = "/pages/join-platform.html";
 }
